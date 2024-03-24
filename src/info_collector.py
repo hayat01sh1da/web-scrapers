70c82f2e87b77cb6ffec9946fbaf5d4fe743f8c3
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-03-25 03:26:51 +0900

    Move in-class methods to the bottom

--- src/info_collector.py
+++ src/info_collector.py
@@ -14,16 +14,8 @@ class InfoCollector:
         self.chrome = webdriver.Chrome(service=Service(os.environ['PATH_TO_WEBDRIVER']), options=options)
         self.list_handler = ListHandler()
 
-    def _get_url(self, base_url, query_str=''):
-        url = '{base_url}{query_str}'.format(base_url = base_url, query_str = query_str)
-        self.chrome.get(url)
-
-    def _get_ranking_items(self):
-        elem_ranking_items = self.chrome.find_elements(By.CLASS_NAME, 'u_categoryTipsItem')
-        return elem_ranking_items
-
     def get_titles(self, base_url, query_str):
-        self._get_url(base_url, query_str)
+        self.__get_url__(base_url, query_str)
         elem_titles = self.chrome.find_elements(By.CLASS_NAME, 'u_title')
         titles      = []
         for elem_title in elem_titles:
@@ -31,7 +23,7 @@ class InfoCollector:
         return titles
 
     def get_evaluations(self, base_url, query_str):
-        self._get_url(base_url, query_str)
+        self.__get_url__(base_url, query_str)
         elem_rank_boxes = self.chrome.find_elements(By.CLASS_NAME, 'u_rankBox')
         evaluations     = []
         for elem_rank_box in elem_rank_boxes:
@@ -39,20 +31,20 @@ class InfoCollector:
         return evaluations
 
     def get_categories(self, base_url):
-        self._get_url(base_url)
-        elem_ranking_items = self._get_ranking_items()
+        self.__get_url__(base_url)
+        elem_ranking_items = self.__get_ranking_items__()
         categories         = self.list_handler.tag_elems_list(elem_ranking_items, 'dt')
         return categories[0]
 
     def get_rankings(self, base_url, query_str):
-        self._get_url(base_url, query_str)
-        elem_ranking_items = self._get_ranking_items()
+        self.__get_url__(base_url, query_str)
+        elem_ranking_items = self.__get_ranking_items__()
         rankings           = self.list_handler.class_elems_list(elem_ranking_items, 'is_rank')
         return rankings
 
     def get_comments(self, base_url, query_str):
-        self._get_url(base_url, query_str)
-        elem_ranking_items = self._get_ranking_items()
+        self.__get_url__(base_url, query_str)
+        elem_ranking_items = self.__get_ranking_items__()
         comments           = self.list_handler.class_elems_list(elem_ranking_items, 'comment')
         return comments
 
@@ -64,3 +56,13 @@ class InfoCollector:
         df_rankings.columns = categories
         df                  = pd.concat([df, df_rankings], axis = 1)
         df.to_csv(path)
+
+    # private
+
+    def __get_url__(self, base_url, query_str=''):
+        url = '{base_url}{query_str}'.format(base_url=base_url, query_str=query_str)
+        self.chrome.get(url)
+
+    def __get_ranking_items__(self):
+        elem_ranking_items = self.chrome.find_elements(By.CLASS_NAME, 'u_categoryTipsItem')
+        return elem_ranking_items
