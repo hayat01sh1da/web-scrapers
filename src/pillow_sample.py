91c83f5bc7e16d4e2ca976bc7b96bea1363f3a1e
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-03-31 02:41:36 +0900

    Make PillowSample safe to absence of specific directory

--- src/pillow_sample.py
+++ src/pillow_sample.py
@@ -1,14 +1,19 @@
 from PIL import Image
+import os
+import sys
+sys.path.append('./src')
+from application import Application
 
-class PillowSample:
-    def __init__(self, path):
-        self.image = Image.open(path)
-
-    def image_size(self):
-        return self.image.size
+class PillowSample(Application):
+    def __init__(self, filepath):
+        super().__init__()
+        self.image = Image.open(filepath)
 
     def resize_image(self, size):
         return self.image.resize(size).size
 
-    def save_image(self, path):
-        self.image.save(path)
+    def save_image(self, dirname, filename):
+        if not os.path.isdir(dirname):
+            os.makedirs(dirname)
+        filepath = os.path.join(dirname, filename)
+        self.image.save(filepath)
--- test/test_pillow_sample.py
+++ test/test_pillow_sample.py
@@ -1,24 +1,29 @@
 import unittest
 import sys
 sys.path.append('./src')
-sys.path.append('./imgs')
+sys.path.append('./test')
 from os import path
 from pillow_sample import PillowSample
+from test_application import TestApplication
 
-class TestPillowSample(unittest.TestCase):
+class TestPillowSample(TestApplication):
     def setUp(self):
-        self.pillow_sample = PillowSample('./imgs/bird.jpg')
+        super().setUp()
+        self.filename      = 'bird{suffix}.jpg'
+        self.filepath      = '{dirname}/{filename}'
+        self.pillow_sample = PillowSample(self.filepath.format(dirname = './imgs', filename = self.filename.format(suffix = '')))
 
-    def test_show_image(self):
-        self.assertEqual((1200, 798), self.pillow_sample.image_size())
+    def test_image_size(self):
+        self.assertEqual((1200, 798), self.pillow_sample.image.size)
 
     def test_resize_image(self):
         self.assertEqual((1024, 768), self.pillow_sample.resize_image((1024, 768)))
 
     def test_save_image(self):
         self.pillow_sample.resize_image((1024, 768))
-        self.pillow_sample.save_image('./imgs/bird_resize.jpg')
-        self.assertEqual(True, path.exists('./imgs/bird_resize.jpg'))
+        filepath = self.filepath.format(dirname = self.dirname, filename = self.filename.format(suffix = '_resized'))
+        self.pillow_sample.save_image(self.dirname, self.filename.format(suffix = '_resized'))
+        self.assertEqual(True, path.exists(filepath))
 
 if __name__ == '__main__':
     unittest.main()
