f638347aa762a0c0b2b7fccf960c74c17df823de
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-04-09 01:57:52 +0900

    Adjust code margins

--- src/application.py
+++ src/application.py
@@ -4,8 +4,8 @@ from selenium.webdriver.chrome.service import Service
 
 class Application:
     def __init__(self):
-        self.chrome    = webdriver.Chrome(service = Service(os.environ['PATH_TO_WEBDRIVER']), options = self.__options__())
-        self.base_url  = 'https://scraping-for-beginner.herokuapp.com'
+        self.chrome   = webdriver.Chrome(service = Service(os.environ['PATH_TO_WEBDRIVER']), options = self.__options__())
+        self.base_url = 'https://scraping-for-beginner.herokuapp.com'
 
     # private
 
