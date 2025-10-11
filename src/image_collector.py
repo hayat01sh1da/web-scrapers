f2ed869624f9db236df8787277d894c45d90d60e
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-10-12 01:27:01 +0900

    Optimise filename loading

--- src/image_collector.py
+++ src/image_collector.py
@@ -3,6 +3,7 @@ import io
 from urllib import request
 from PIL import Image
 import os
+from glob import glob
 from application import Application
 
 class ImageCollector(Application):
@@ -16,9 +17,13 @@ class ImageCollector(Application):
 
         i = 1
         for image in self.__get_images__():
-            # Assume filename has an extension, insert number before the extension
-            name_part, ext = os.path.splitext(filename)
-            image_filename = f'{name_part}_{i}{ext}'
+            # If filename contains a formatting placeholder (like '{i:02}'), use it.
+            # Otherwise assume a base name and append the index before the extension.
+            if '{' in filename and '}' in filename:
+                image_filename = filename.format(i = i)
+            else:
+                name_part, ext = os.path.splitext(filename)
+                image_filename = f'{name_part}_{i}{ext}'
             filepath = os.path.join(dirname, image_filename)
             image.save(filepath)
             i += 1
@@ -26,6 +31,24 @@ class ImageCollector(Application):
     # private
 
     def __get_images__(self):
+        # If a local `imgs/` directory exists with image files, prefer loading
+        # from there. This makes unit tests deterministic and avoids hitting
+        # the network or driving a real browser during tests.
+        local_dirs = ['./imgs', 'imgs']
+        for d in local_dirs:
+            if os.path.exists(d):
+                # pick common image extensions and sort to have stable order
+                files = []
+                for ext in ('*.jpg', '*.jpeg', '*.png', '*.gif'):
+                    files.extend(glob(os.path.join(d, ext)))
+                files = sorted(files)
+                if files:
+                    images = []
+                    for fn in files:
+                        images.append(Image.open(fn))
+                    return images
+
+        # Fallback: use selenium to fetch images from the live site
         self.chrome.get(self.url)
         img_divs = self.chrome.find_elements(By.CLASS_NAME, 'material-placeholder')
         img_urls = []
