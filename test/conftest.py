b4eef23a0d63f1b52642911bdadb828631679995
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-05-18 16:43:32 +0900

    Update regular expression to delete Python-related caches after running unit test in callback

--- test/conftest.py
+++ test/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-import glob
+import re
 import os
 import shutil
 import sys
@@ -11,17 +11,13 @@ sys.path.append('./src/lib')
 
 @pytest.fixture(autouse=True)
 def __cleanup_caches__() -> Iterator[None]:
-    before = set(
-        glob.glob(
-            os.path.join(
-                '.',
-                '**',
-                '__pycache__'),
-            recursive=True))
     yield
-    for pycache in before:
-        if os.path.exists(pycache):
-            shutil.rmtree(pycache)
+    cache_dir = re.compile(r'^(?:__pycache__|\.pytest_cache|\.mypy_cache)$')
+    for root, dirs, _ in os.walk('.'):
+        for name in list(dirs):
+            if cache_dir.match(name):
+                shutil.rmtree(os.path.join(root, name), ignore_errors=True)
+                dirs.remove(name)
 
 
 @pytest.fixture
